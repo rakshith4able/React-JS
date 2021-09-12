fa5cbf935f17beb155d613ba98aac2df1d762ef8
Rakshith Raj G P <rakshithraj.gp11@gmail.com>
2021-09-12 21:01:40 +0530

    Starting with Components

---
+++ Basics/Components/app.js
@@ -0,0 +1,4 @@
+/*
+- Components are the main building blocks of react
+- React helps us to make components, communicate bw the components and pass data,information and methods bw one another
+- Components let us split the UI into independent,reusable pieces
