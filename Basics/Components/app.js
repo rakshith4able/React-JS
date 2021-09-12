68e365f2dc33118d06f12d464f14694f6fc0c99b
Rakshith Raj G P <rakshithraj.gp11@gmail.com>
2021-09-12 21:14:12 +0530

    Setting Up server

--- Basics/Components/app.js
+++ Basics/Components/app.js
@@ -2,3 +2,14 @@
 - Components are the main building blocks of react
 - React helps us to make components, communicate bw the components and pass data,information and methods bw one another
 - Components let us split the UI into independent,reusable pieces
+
+TRADITIONAL WAY OF USING REACT JS
+
+-WE NEED A SERVER
+-WE CAN DO IT IN MANY WAYS
+-DOWNLOAD EXTENSION LIVE SERVER BY RITWICK DEV IN VS CODE
+-AFTER INSTALLING WE NEED TO MAKE SURE THAT WE HAVE A DIRECTORY OPEN
+-RIGHT CLICK ON HTML WINDOW AND CLICK ON OPEN WITH LIVE SERVER
+-THEN THE PAGE OPENS IN SERVER
+-IF WE CHANGE SOMETHING IN A PAGE WE DONT HAVE TO REFRESH , IT AUTOMATICALLY CHANGES
+*/
